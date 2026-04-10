79f2432c398061949796f4722f10c1399de10da8
Josué Luís Pimenta Alecrim <JosueAlecrim7@gmail.com>
2026-04-10 11:27:30 -0300

    Add files via upload

---
+++ App.java
@@ -0,0 +1,28 @@
+
+import java.util.Scanner;
+
+
+public class App {
+   
+public static String maior( int a, int b) {
+
+    if (a > b) {
+        return a + " é maior";
+    } else {
+        return b + " é maior";
+    }
+
+}
+public static void main(String[]args) {
+
+    Scanner input = new Scanner(System.in);
+
+    System.out.println("Informe os valores");
+    int a = input.nextInt();
+    int b = input.nextInt();
+  
+    String resultado = maior(a,b);
+    System.out.println(resultado);
+    input.close();
+  }
+}
